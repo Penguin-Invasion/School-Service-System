566add3d95ad59fc957e0229381e1cef4c73926a
Baran Solmaz <b.solmaz2018@gtu.edu.tr>
2021-12-03 00:35:16 +0300

    Add Bus DTO

---
+++ backend/SchoolServiceSystem/SchoolServiceSystem/DTOs/Bus/CreateBusDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SchoolServiceSystem.DTOs.Bus
+{
+    public class CreateBusDTO
+    {
+        public int DriverID { get; set; }
+        public string Plaque { get; set; }
+    }
+}
