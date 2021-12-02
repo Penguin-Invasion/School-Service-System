67949b82361c0c1ee71c78eb7087fe0254b0404d
Enis Yalçın <enisyalcin96@hotmail.com>
2021-12-02 23:24:17 +0300

    Create SSSExceptions

---
+++ backend/SchoolServiceSystem/SchoolServiceSystem/Exceptions/NotCreatedException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SchoolServiceSystem.Exceptions
+{
+    public class NotCreatedException : SSSException
+    {
+        public NotCreatedException(string message = null) : base(message)
+        {
+
+        }
+    }
+}
