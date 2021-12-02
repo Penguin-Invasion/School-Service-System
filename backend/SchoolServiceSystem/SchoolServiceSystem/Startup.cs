49d093c06073c08941633f5b64b02d5936bea102
Enis Yalçın <enisyalcin96@hotmail.com>
2021-12-02 23:22:35 +0300

    fix

--- backend/SchoolServiceSystem/SchoolServiceSystem/Startup.cs
+++ backend/SchoolServiceSystem/SchoolServiceSystem/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using SchoolServiceSystem.Data;
+using SchoolServiceSystem.Filters;
 using System;
 using System.Collections.Generic;
 using System.Linq;
