fce6020f759d6218aaccb311fc4ad212534f285f
Enis Yalçın <enisyalcin96@hotmail.com>
2021-12-03 18:21:35 +0300

    Add Auth DTO

---
+++ backend/SchoolServiceSystem/SchoolServiceSystem/DTOs/Auth/GetUserWithTokenDTO.cs
@@ -0,0 +1,13 @@
+﻿using SchoolServiceSystem.DTOs.User;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SchoolServiceSystem.DTOs.Auth
+{
+    public class GetUserWithTokenDTO : GetUserDTO
+    {
+        public string Token;
+    }
+}
