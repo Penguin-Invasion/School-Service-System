843d604bfa5e71d8d6ab024636cb4650ecb62227
Omer <hsynmrgry@gmail.com>
2021-12-07 00:47:34 +0300

    OGUZ BABA

--- frontend/src/components/Footers/AdminFooter.js
+++ frontend/src/components/Footers/AdminFooter.js
@@ -50,35 +50,7 @@ const Footer = () => {
               </NavLink>
             </NavItem>
 
-            <NavItem>
-              <NavLink
-                href="https://www.creative-tim.com/presentation?ref=adr-admin-footer"
-                rel="noopener noreferrer"
-                target="_blank"
-              >
-                About Us
-              </NavLink>
-            </NavItem>
-
-            <NavItem>
-              <NavLink
-                href="http://blog.creative-tim.com?ref=adr-admin-footer"
-                rel="noopener noreferrer"
-                target="_blank"
-              >
-                Blog
-              </NavLink>
-            </NavItem>
-
-            <NavItem>
-              <NavLink
-                href="https://github.com/creativetimofficial/argon-dashboard/blob/master/LICENSE.md?ref=adr-admin-footer"
-                rel="noopener noreferrer"
-                target="_blank"
-              >
-                MIT License
-              </NavLink>
-            </NavItem>
+           
           </Nav>
         </Col>
       </Row>
