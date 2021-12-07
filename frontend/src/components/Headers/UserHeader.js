04a97655fb822dd2579c9724c97868c37ccd0a3b
Muhammed Oguz <m.oguz2018@gtu.edu.tr>
2021-12-07 14:40:08 +0300

    Change profile photo

--- frontend/src/components/Headers/UserHeader.js
+++ frontend/src/components/Headers/UserHeader.js
@@ -22,7 +22,7 @@ const UserHeader = () => {
         <Container className="d-flex align-items-center" fluid>
           <Row>
             <Col lg="7" md="10">
-              <h1 className="display-2 text-white">Hello Jesse</h1>
+              <h1 className="display-2 text-white">Hello Habil</h1>
               <p className="text-white mt-0 mb-5">
                 This is your profile page. You can see your profile info and you can edit it. You can also see your image and can change it.
               </p>
