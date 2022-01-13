43ee2579db33a388c5e74f1a287e441425d4f087
= <=>
2022-01-13 15:48:10 +0300

    image processing last code added

--- Image Processing/main.py
+++ Image Processing/main.py
@@ -1,32 +1,29 @@
-from numpy.lib.npyio import recfromtxt
 import requests
 import cv2
 import pytesseract
 import imutils
 import numpy as np
+import re
+import pystray
+from pystray import MenuItem as item
 from tkinter import *
 from PIL import Image,ImageTk
+import time
 
 #Starting Optical Character Recognition Software
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files (x86)\Tesseract-OCR\tesseract.exe'
 
 def applyFilters(img):
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) #Convert to gray scale
-    gray = cv2.bilateralFilter(gray, 13, 30, 30) #Apply blur 25 was good
-    return gray
+    return cv2.bilateralFilter(gray, 13, 30, 30)
 
 def detectContours(img):
     img = cv2.Canny(img, 30, 200) #Apply Canny Edge Detection
     contours=cv2.findContours(img.copy(),cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     contours = imutils.grab_contours(contours)
-    contours = sorted(contours,key=cv2.contourArea, reverse = True)[:10]
-    return contours
+    return sorted(contours,key=cv2.contourArea, reverse = True)[:10]
 
 def detectPlate(contours):
-    global recx
-    global recy 
-    global recw 
-    global rech
     screenCnt = np.empty( shape=(0, 0) )
     for c in contours:
         peri = cv2.arcLength(c, True) # approximate the contour
@@ -34,71 +31,116 @@ def detectPlate(contours):
         if len(approx) == 4: # if our approximated contour has four points, then it's a rectangle which is probably the plate
             screenCnt = approx
             recx,recy,recw,rech = cv2.boundingRect(approx)
+            Rect[0],Rect[1],Rect[2],Rect[3] = recx,recy,recw,rech
             break
     return screenCnt
 
 def cropPlate(screenCnt,gray,img):
-    global topx
-    global topy
-    global bottomx
-    global bottomy
     mask = np.zeros(gray.shape,np.uint8)
     new_image = cv2.drawContours(mask,[screenCnt],0,255,-1,)
     new_image = cv2.bitwise_and(img,img,mask=mask)
     (x, y) = np.where(mask == 255)
     (topx, topy) = (np.min(x), np.min(y))
     (bottomx, bottomy) = (np.max(x), np.max(y))
-    cropped = gray[topx:bottomx+1, topy:bottomy+1]
-    #threshold,thresh = cv2.threshold(cropped,75,255,cv2.THRESH_BINARY)
-    return cropped
+    #Binary thresholding may be useful
+    return gray[topx:bottomx+1, topy:bottomy+1]
 
 def applyOcr(img) : return pytesseract.image_to_string(img, config='--psm 11').replace("\n","")
 
-recx,recy,recw,rech = 0,0,0,0
-topx,topy,bottomx,bottomy = 0,0,0,0
-window = Tk() #instantiate an instance of a window
-window.geometry("1280x600")
-window.title("School Service System")
-window.config(background="#000000")
-icon = PhotoImage(file='icon.png')
-label = Label(window,image=icon,bg = "#000000",borderwidth=0,highlightthickness=0)
-textCompany = Label(window,text= "School Service System",font=('Arial',35),fg="#FFFFFF",bg = "#000000",borderwidth=0,highlightthickness=0)
-label.place(x=50,y=50)
-textCompany.place(x=40,y=375)
-window.iconphoto(True,icon)
-url = "http://192.168.1.24:8080/shot.jpg"
-textPlate = Label(window)
-while True:
+def createWindow():
+    window.geometry("1280x600")
+    window.title("School Service System")
+    window.config(background="#000000")
+    label = Label(window,image=icon,bg = "#000000",borderwidth=0,highlightthickness=0)
+    textCompany = Label(window,text= "School Service System",font=('Arial',35),fg="#FFFFFF",bg = "#000000",borderwidth=0,highlightthickness=0)
+    label.place(x=50,y=50)
+    textCompany.place(x=40,y=375)
+    window.iconphoto(True,icon)
+
+def getImageFromCamera():
     img_resp = requests.get(url)
     img_arr = np.array(bytearray(img_resp.content), dtype=np.uint8)
     img = cv2.imdecode(img_arr, -1)
-    img = imutils.resize(img, width=620, height=480)
-    if cv2.waitKey(1) == 27:
-        break
-    else:
-        gray = applyFilters(img)
-        contours = detectContours(gray)
-        screenCnt = detectPlate(contours)
-        if(screenCnt.size == 0) : print("There is no plate")
-        else:
-            plateImg = cropPlate(screenCnt,gray,img)
-            plate = applyOcr(plateImg)
-            if(len(plate) > 0 ):
-                msg = "Detected plate: " + plate
-                textPlate = Label(window,text= msg,font=('Arial',30),fg="#FFFFFF",bg = "#000000",borderwidth=0,highlightthickness=0)
-                textPlate.place(x=600,y=450)
-                cv2.rectangle(img,(recx,recy),(recx + recw,recy + rech),(0,255,0),2)
-                pass
+    return imutils.resize(img, width=620, height=480)
+
+def showPlateInfo(plate):
+    global textPlate
+    msg = "Detected plate: " + plate
+    textPlate = Label(window,text= msg,font=('Arial',30),fg="#FFFFFF",bg = "#000000",borderwidth=0,highlightthickness=0)
+    textPlate.place(x=600,y=450)
+
+def convertImage(img):
+    b,g,r = cv2.split(img)
+    img = cv2.merge((r,g,b))
+    return Image.fromarray(img)
+
+def removeInvalidCharacters(str):
+    rtr = ""
+    for i in str: 
+        if(i.isalnum()) : rtr += i
+    return rtr
+
+def isPlate(str):
+    regex = "([0-9]{2})([A-Z]{1,3})([0-9]{2,4})"
+    if(re.search(regex,str) == None):  return FALSE
+    return TRUE
+    
+# Define a function for quit the window
+def quit_window(icon, item):
+   icon.stop()
+   window.destroy()
+
+# Define a function to show the window again
+def show_window(icon, item):
+   icon.stop()
+   window.deiconify()
+
+
+def closeButton():
+    window.withdraw()
+    image=Image.open("image.ico")
+    menu = pystray.Menu(item('Show', show_window, default=True),item('Quit', quit_window))
+    icon=pystray.Icon("School Service System", image, "School Service System", menu)
+    icon.run()
+
+    
+
+Rect = [0,0,0,0]
+gui = TRUE
+window = Tk() #instantiate an instance of a window
+window.protocol("WM_DELETE_WINDOW", closeButton)
+plates = []
+times = []
+plateList = [times,plates]
+icon = PhotoImage(file='icon.png')
+textPlate = Label(window)
+window.overrideredirect()
+createWindow()
+with open("IpCameraUrl.txt","r") as file : url = file.read()
+while True:
+    img = getImageFromCamera()
+    gray = applyFilters(img)
+    contours = detectContours(gray)
+    screenCnt = detectPlate(contours)
+    if(screenCnt.size > 0):
+        plateImg = cropPlate(screenCnt,gray,img)
+        scannedText = removeInvalidCharacters(applyOcr(plateImg))
+        if(isPlate(scannedText)): 
+            showPlateInfo(scannedText)
+            cv2.rectangle(img,(Rect[0],Rect[1]),(Rect[0] + Rect[2],Rect[1] + Rect[3]),(0,255,0),2)
+            scannedText = "34A1234"
+            if(scannedText in plateList[1]):
+                for i in range(len(plateList[1])):
+                    if(plateList[1][i] == scannedText):
+                        if( time.time() - plateList[0][i] > 60): 
+                            plateList[0][i] = time.time()
+                            requests.post("https://ce781c9c20994b.localhost.run/api/entry?SecretKey=737VI4Z8BZK1TOEMV3WOB9WNU9LGQ1&Plaque=" + scannedText)
+                            break
             else:
-                print("Read nothing from cropped image")
-        b,g,r = cv2.split(img)
-        img = cv2.merge((r,g,b))
-        im = Image.fromarray(img)
-        guimage = ImageTk.PhotoImage(image=im) 
-        imglabel = Label(window,image=guimage,bg = "#000000",borderwidth=0,highlightthickness=0)
-        imglabel.place(x=600,y=50)
-        window.update()
-        textPlate.place(x=-500,y=-500)
-        
-  
-cv2.destroyAllWindows()
+                plateList[0].append(time.time())
+                plateList[1].append(scannedText)
+                requests.post("https://ce781c9c20994b.localhost.run/api/entry?SecretKey=737VI4Z8BZK1TOEMV3WOB9WNU9LGQ1&Plaque=" + scannedText)
+    guimage = ImageTk.PhotoImage(image=convertImage(img)) 
+    Label(window,image=guimage,bg = "#000000",borderwidth=0,highlightthickness=0).place(x=600,y=50)
+    window.update()
+    textPlate.place(x=-500,y=-500)
