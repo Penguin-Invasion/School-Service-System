64c0023d747c1dba9596dfe299c2311a6d9d5b72
Muhammed Oguz <m.oguz2018@gtu.edu.tr>
2022-01-18 16:50:12 +0300

    Fetch info for services from backend

--- frontend/src/components/ServiceCard/ServiceBodyContainer.js
+++ frontend/src/components/ServiceCard/ServiceBodyContainer.js
@@ -4,6 +4,20 @@ import { useState, useEffect } from 'react'
 
 import useToken from '../../useToken'
 
+const getEntries = async (schoolId, serviceId, token) => {
+    const result = await fetch('https://schoolservicesystem.azurewebsites.net/api/School/' + schoolId + '/Service/' + serviceId, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + token
+        }
+    })
+
+    const body = await result.json()
+    console.log("body? ", body)
+    return body.data.entries
+}
+
 const ServiceBodyContainer = (props) => {
     const [serviceBody, setServiceBody] = useState([])
     const [serviceEntries, setServiceEntries] = useState([])
@@ -40,18 +54,30 @@ const ServiceBodyContainer = (props) => {
             // loop through the services and push the data into the array
             for (let i = 0; i < allServices.length; i++) {
                 const name = allServices[i].name
-                const entries = allServices[i].entries
+                const plaque = allServices[i].plaque
+                const entries = await getEntries(body.data[0].id, allServices[i].id, token)
+                console.log("i: " + i + " name: " + name + " entries: " + entries)
 
                 for (let j = 0; j < entries.length; j++) {
                     serviceEntries.push({
                         id: entries[j].id,
                         name: name,
+                        plaque: plaque,
                         time: entries[j].time,
                         date: entries[j].date,
                     })
 
                 }
             }
+
+            // sort serviceEntries by date and time
+            serviceEntries.sort((a, b) => {
+                const dateA = new Date(a.date)
+                const dateB = new Date(b.date)
+                return dateB - dateA
+            })
+
+            
 
             console.log("serviceEntries: ", serviceEntries)
             setServiceEntries(serviceEntries)
