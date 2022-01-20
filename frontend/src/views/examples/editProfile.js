1b3ddf3baa0304bac7b9de1c01860916aa594cf8
Muhammed Oguz <m.oguz2018@gtu.edu.tr>
2022-01-21 02:32:13 +0300

    Change profile duzenle with better ui

--- frontend/src/views/examples/editProfile.js
+++ frontend/src/views/examples/editProfile.js
@@ -71,50 +71,103 @@ import {
         <Container className="mt--7  " fluid>
           <Row>
             <Col className="order-xl-1" xl="8">
+            <CardHeader className="bg-white border-0">
+                  <Row className="align-items-center">
+                    <Col xs="8">
+                      <h3 className="mb-0">Profil Düzenle</h3>
+                    </Col>
+                    
+                  </Row>
+                </CardHeader>
               <Card className="bg-secondary shadow">
                 <CardBody>
                 <form onSubmit={handleSubmit}>
-                <p>Bu ekrandan, bilgilerinizi güncelleyebilirsiniz. Sadece değişmesini istdiğiniz bilgileri gönderebilirsiniz.</p>
-                    <div className="mb-3" >
-                        <label htmlFor="name">Isim</label>
-                        <input
-                        className="form-control"
-                        id="name"
-                        type="text"
-                        value={name}
-                        onChange={(e) => setName(e.target.value)}
-                        />
-                    </div>
-
-                    <div className="mb-3" >
-                        <label htmlFor="lastName">Soyism</label>
-                        <input
-                        className="form-control"
-                        id="lastName"
-                        type="text"
-                        value={lastName}
-                        onChange={(e) => setLastName(e.target.value)}
-                        />
-                    </div>
-                    <div className="mb-3" >
-                        <label htmlFor="email">E-Mail</label>
-                        <textarea
-                        className="form-control"
-                        id="email"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                        />
-                    </div>
-                    <div className="mb-3" >
-                        <label htmlFor="userName">Yeni Şifre</label>
-                        <input
-                        className="form-control"
-                        id="userName"
+                <h6 className="heading-small text-muted mb-4">
+                Bu ekrandan, bilgilerinizi güncelleyebilirsiniz. Sadece değişmesini istdiğiniz bilgileri gönderebilirsiniz.
+                </h6>
+                <div className="pl-lg-4">
+                      <Row>
+                        <Col lg="6">
+                          <FormGroup>
+                            <label
+                              className="form-control-label"
+                              htmlFor="input-username"
+                            >
+                                Ad
+                            </label>
+                            <Input
+                              className="form-control-alternative"
+                              id="name"
+                              placeholder="Ad"
+                            type="text"
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
+                            />
+                          </FormGroup>
+                        </Col>
+                        <Col lg="6">
+                        <FormGroup>
+                            <label
+                              className="form-control-label"
+                              htmlFor="input-last-name"
+                            >
+                              Soyad
+                            </label>
+                            <Input
+                              className="form-control-alternative"
+                              
+                              placeholder="Soyad"
+                              id="lastName"
+                            type="text"
+                            value={lastName}
+                            onChange={(e) => setLastName(e.target.value)}
+                            />
+                          </FormGroup>
+                        </Col>
+                      </Row>
+                      <Row>
+                        <Col lg="6">
+                        <FormGroup>
+                            <label
+                              className="form-control-label"
+                              htmlFor="input-email"
+                            >
+                              E Posta Adresi
+                            </label>
+                            <Input
+                              className="form-control-alternative"
+                              id="input-email"
+                              placeholder="ornek@gmail.com"
+                              type="text"
+                              id="email"
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                              
+                            />
+                          </FormGroup>
+                        </Col>
+                        <Col lg="6">
+                          <FormGroup>
+                            <label
+                              className="form-control-label"
+                              htmlFor="input-last-name"
+                            >
+                              Yeni Şifre
+                            </label>
+                            <Input
+                              className="form-control-alternative"
+                              
+                              placeholder="Şifre"
+                              id="userName"
                         type="password"
                         value={password}
                         onChange={(e) => setPassword(e.target.value)}
-                        />
+                            />
+                          </FormGroup>
+                        </Col>
+                      </Row>
                     </div>
+
 
                     <div>
                        
