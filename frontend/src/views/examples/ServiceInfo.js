a262ea743c9c57040509c7585b09d5e594f7142e
Muhammed Oguz <m.oguz2018@gtu.edu.tr>
2022-01-21 07:28:00 +0300

    Complete add student functionality

--- frontend/src/views/examples/ServiceInfo.js
+++ frontend/src/views/examples/ServiceInfo.js
@@ -1,33 +1,15 @@
 import Header from "components/Headers/Header.js";
 
-import { Card, CardBody, Button, Col, Container, CardHeader, Row, Table } from 'reactstrap';
+import { Form, FormGroup, Input, Card, CardBody, Button, Col, Container, CardHeader, Row, Table, Label } from 'reactstrap';
 
 import { useState, useEffect } from "react";
 
 import useToken from '../../useToken'
 
-const a = async (schoolId, serviceId, token)  => {
-    const result = await fetch('https://schoolservicesystem.azurewebsites.net/api/School/' + schoolId + '/Service/' + serviceId, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer ' + token
-        }
-    })
 
-    const body = await result.json()
 
-    console.log("log?")
 
-    if (body.success) {
-        console.log("body??????", body);
-        setServiceName(body.data.name);
-        setServicePlaque(body.data.plaque);
-        setDriverName(body.data.driver.name + ' ' + body.data.driver.surName);
-        setStudents(body.data.students);
-    }
-
-}
+                
 
 const deleteService = async (schoolId, serviceId, token) => {
     const result = await fetch('https://schoolservicesystem.azurewebsites.net/api/School/' + schoolId + '/Service/' + serviceId, {
@@ -51,8 +33,16 @@ const ServiceInfo = (props) => {
     const [ students, setStudents ] = useState([]);
     const { token } = useToken();
 
+    const [ renderStudent, setRenderStudent ] = useState(false);
+
     const schoolId = props.match.params.schoolId;
     const serviceId = props.match.params.id;
+
+    const [ buttonValue, setButtonValue ] = useState('Yeni Öğrenci Ekle');
+
+    const [ studentName, setStudentName ] = useState('');
+    const [ studentSurname, setStudentSurname ] = useState('');
+    const [ studentYear, setStudentYear ] = useState('');
 
 
     const getService = async ()  => {
@@ -79,6 +69,51 @@ const ServiceInfo = (props) => {
         getService();
     }, [])
 
+    const renderStudentForm = () => {
+        setRenderStudent(!renderStudent);
+        if (!renderStudent) {
+            setButtonValue('Öğrenci Eklemeyi Bitir');
+        }
+        else {
+            setButtonValue('Yeni Öğrenci Ekle');
+        }
+    }
+
+    const addStudent = async () => {
+        const result = await fetch('https://schoolservicesystem.azurewebsites.net/api/School/' + schoolId + '/Service/' + serviceId + '/Student', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + token
+            },
+            body: JSON.stringify({
+                name: studentName,
+                surname: studentSurname,
+                year: studentYear
+            })
+        })
+
+        const body = await result.json()
+
+    }
+
+    const deleteStudent = async (studentId) => {
+        console.log("studentId", studentId)
+        const result = await fetch('https://schoolservicesystem.azurewebsites.net/api/School/' + schoolId + '/Service/' + serviceId + '/Student/' + studentId, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + token
+            }
+        })
+
+        const body = await result.json()
+    }
+
+
+
+
+
 
     return (
         <>
@@ -96,6 +131,7 @@ const ServiceInfo = (props) => {
         <Col className="text-right" xs="4">
             <Button onClick={() => deleteService(schoolId, serviceId, token)} color="warning" size="sm">Servisi Kaldır</Button>
         </Col>
+        
         </Row>
         </CardHeader>
         <Card  className="bg-success shadow">
@@ -123,8 +159,59 @@ const ServiceInfo = (props) => {
             <p>Öğrenci bilgileri listelenmektedir. Yeni Öğrenci eklemek için butonu kullanabilirsiniz.</p>
         </Col>
         <Col className="text-right" xs="4">
-            <Button color="info" size="sm">Yeni Öğrenci Ekle</Button>
+            <Button onClick={renderStudentForm} color="info" size="sm">{ buttonValue }</Button>
         </Col>
+        {// contidional rendering
+        renderStudent ?
+        <>
+            <Form>
+                <Row className="align-items-center">
+                    <Col>
+                        <FormGroup>
+                        <Label for="studentName">Öğrenci Adı</Label>
+                            <Input
+                                type="text"
+                                name="name"
+                                id="name"
+                                placeholder="İsim"
+                                value={studentName}
+                                onChange={(e) => setStudentName(e.target.value)}
+                            />
+                        </FormGroup>
+                    </Col>
+                    <Col>
+                        <FormGroup>
+                        <Label for="studentSurname">Öğrenci Soyadı</Label>
+                            <Input
+                                type="text"
+                                name="lastName"
+                                id="lastName"
+                                placeholder="Soyisim"
+                                value={studentSurname}
+                                onChange={(e) => setStudentSurname(e.target.value)}
+                            />
+                        </FormGroup>
+                    </Col>
+                    <Col>
+                        <FormGroup>
+                        <Label for="studentYear">Öğrenci Giriş Yılı</Label>
+                            <Input
+                                type="email"
+                                name="email"
+                                id="email"
+                                placeholder="Giriş Yılı"
+                                value={studentYear}
+                                onChange={(e) => setStudentYear(e.target.value)}
+                            />
+                        </FormGroup>
+                    </Col>
+                    <Col>
+                    <Button onClick={addStudent} className="bg-info" >Kaydet</Button>
+                    </Col>
+                </Row>
+            </Form>
+        </> : null}
+        
         </Row>
     </CardHeader>
     <Table className="table-content-color-student" borderless>
@@ -144,7 +231,7 @@ const ServiceInfo = (props) => {
                     <td>{student.name}</td>
                     <td>{student.surName}</td>
                     <td>{student.year}</td>
-                    <td><Button color="danger" size="sm">Sil</Button></td>
+                    <td><Button onClick={() => deleteStudent(student.id)} color="danger" size="sm">Sil</Button></td>
                     </tr>
                 )
             })}
